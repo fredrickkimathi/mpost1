edb8550d1536318e4125d31a87984a3d15d840f9
yourboyfred <fredrickkimathi26@gmail.com>
2024-05-08 16:35:09 +0300

    working registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -22,9 +24,13 @@ class RegisterController extends Controller
 
         ]);
 
-        dd('store');
-
         //store
+        User::create([
+            'name' =>$request->name,
+            'username'=>$request->username,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password),
+        ]);
         //signin
         //redirect
     }
--- database/migrations/2024_05_08_133031_create_users_table.php
+++ database/migrations/2024_05_08_133031_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -34,3 +35,4 @@ class CreateUsersTable extends Migration
         Schema::dropIfExists('users');
     }
 }
+
