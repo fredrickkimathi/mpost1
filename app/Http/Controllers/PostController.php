a24ddb614d7d5e48c77355095598abbaccc8fa1f
yourboyfred <fredrickkimathi26@gmail.com>
2024-05-09 14:19:26 +0300

    eager loading

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::paginate(5); //returns all posts in the database in order
+        $posts = Post::with(['user', 'likes'])->paginate(5); //returns all posts in the database in order
         return view('posts.index', [
             'posts' => $posts
         ]);
